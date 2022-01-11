beaa7e050dd30518e0c3141a62bcc38662978547
Ilham Akbari <ilham@eannovate.com>
2022-01-11 10:36:40 +0700

    Bug fix url submenu IRJ

--- application/views/admin/module/service/irj/sub_menu/index.php
+++ application/views/admin/module/service/irj/sub_menu/index.php
@@ -40,7 +40,7 @@
                         <a class="nav-link" href="<?=base_url('administrator/irj')?>">Description</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" href="<?=base_url('administrator/irj/sub_menu/')?>">Sub Menu</a>
+                        <a class="nav-link active" href="<?=base_url('administrator/irj/sub_menu/').$service_id.'/'?>">Sub Menu</a>
                       </li>
 
                       <li class="nav-item">
