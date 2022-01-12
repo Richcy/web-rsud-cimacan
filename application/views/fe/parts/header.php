6000e3d342b5a4df0fd2e44ab415814489f50ec0
Ilham Akbari <ilham@eannovate.com>
2022-01-12 11:12:45 +0700

    Adjust header for mobile

--- application/views/fe/parts/header.php
+++ application/views/fe/parts/header.php
@@ -4,7 +4,7 @@
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <h1 class="logo me-auto"><a href="index.html">Medicio</a></h1> -->
 
-      <nav id="navbar" class="navbar order-lg-0">
+      <nav class="navbar order-lg-0 hidden-xs hidden-sm">
         <ul>
           <li><a class="nav-link <?=$cur_page=='home' || $this->uri->segment(1)=='' ? 'active' : '';?>" href="<?=base_url()?>">Beranda</a></li>
 
@@ -43,6 +43,56 @@
               }
             }
            ?>
+          <li class="dropdown">
+            <a href="javascript:void();" class="nav-parent <?=$dropdown_parent_layanan;?>"><span>Layanan</span>
+              <i class="bi bi-chevron-down"></i>
+            </a>
+            <ul>
+              <!-- <li><a class="<?=$cur_page=='layanan_unggulan' ? 'active' : '';?>" href="<?=base_url('layanan-unggulan.html')?>">Layanan Unggulan</a></li> -->
+
+              <li class="navbar-child"><a class="<?=$cur_page=='layanan_unggulan' ? 'active' : '';?>" href="<?=base_url('superior-service.html')?>">Layanan Unggulan</a></li>
+              <li class="navbar-child"><a class="<?=$cur_page=='instalasi_rawat_jalan' ? 'active' : '';?>" href="<?=base_url('outpatient-installation.html')?>">Instalasi Rawat Jalan</a></li>
+              <li class="navbar-child"><a class="<?=$cur_page=='instalasi_rawat_inap' ? 'active' : '';?>" href="<?=base_url('inpatient-installation.html')?>">Instalasi Rawat Inap</a></li>
+              <li class="navbar-child"><a class="<?=$cur_page=='igd' ? 'active' : '';?>" href="<?=base_url('igd.html')?>">Instalasi Gawat Darurat</a></li>
+              <li class="navbar-child"><a class="<?=$cur_page=='laboratorium' ? 'active' : '';?>" href="<?=base_url('laboratorium.html')?>">Laboratorium</a></li>
+              <li class="navbar-child"><a class="<?=$cur_page=='radiology' ? 'active' : '';?>" href="<?=base_url('radiology.html')?>">Radiologi</a></li>
+              <!-- <li class="navbar-child"><a class="<?=$cur_page=='medical_support' ? 'active' : '';?>" href="<?=base_url('medical-support.html')?>">Penunjang Medis</a></li> -->
+            </ul>
+          </li>
+          <!--END Dropdown Menu Layanan -->
+          <!-- <li><a class="nav-link" href="javascript:void();">Departemen</a></li> -->
+          <li><a class="nav-link <?=$cur_page=='doctor' ? 'active' : '';?>" href="<?=base_url('doctor/');?>">Dokter</a></li>
+          <li><a class="nav-link <?=$cur_page=='event' ? 'active' : '';?>" href="<?=base_url('event/');?>">Event</a></li>
+          <li><a class="nav-link <?=$cur_page=='article' ? 'active' : '';?>" href="<?=base_url('article/');?>">Artikel</a></li>
+          <?php if ($cur_page == 'home') {?>
+            <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+          <?php }else{ ?>
+          <li><a class="nav-link <?=$cur_page=='contact' ? 'active' : '';?>" href="<?=base_url('contact.html')?>">Kontak</a></li>
+          <?php } ?>
+          <li><a class="nav-link <?=$cur_page=='career' ? 'active' : '';?>" href="<?=base_url('career/')?>">Karir</a></li>
+        </ul>
+        <!-- <i class="bi bi-list mobile-nav-toggle"></i> -->
+      </nav><!-- .navbar -->
+
+
+      <!-- Mobile Menu -->
+      <nav id="navbar" class="navbar order-lg-0 hidden-lg hidden-md">
+        <ul>
+          <li><a class="nav-link <?=$cur_page=='home' || $this->uri->segment(1)=='' ? 'active' : '';?>" href="<?=base_url()?>">Beranda</a></li>
+          <li class="dropdown">
+            <a href="javascript:void();" class="nav-parent <?=$dropdown_parent_tentang;?>"><span>Tentang</span>
+              <i class="bi bi-chevron-down"></i>
+            </a>
+            <ul>
+              <li class="navbar-child"><a class="<?=$cur_page=='profil_rs' ? 'active' : '';?>" href="<?=base_url('profile.html')?>">Profil Rumah Sakit</a></li>
+
+              <li class="navbar-child"><a class="<?=$cur_page=='directurs_greeting' ? 'active' : '';?>" href="<?=base_url('directurs-greeting.html')?>">Sambutan Direktur</a></li>
+
+              <li class="navbar-child"><a class="<?=$cur_page=='structure_or' ? 'active' : '';?>" href="<?=base_url('structure.html')?>">Struktur Organisasi</a></li>
+
+              <li class="navbar-child"><a class="<?=$cur_page=='denah_rs' ? 'active' : '';?>" href="<?=base_url('sketch.html')?>">Denah Rumah Sakit</a></li>
+            </ul>
+          </li>
           <li class="dropdown">
             <a href="javascript:void();" class="nav-parent <?=$dropdown_parent_layanan;?>"><span>Layanan</span>
               <i class="bi bi-chevron-down"></i>
