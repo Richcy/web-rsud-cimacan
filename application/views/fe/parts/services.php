74d7e2be4659bb1ff31bb6eb6bb968dceb880dde
Ilham Akbari <ilham@eannovate.com>
2022-01-19 08:57:58 +0700

    Adjust view

--- application/views/fe/parts/services.php
+++ application/views/fe/parts/services.php
@@ -7,27 +7,39 @@
 
     <div class="row">
       <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="300">
+        <a href="<?=base_url('superior-service.html')?>">
         <div class="icon"><i class="fas fa-notes-medical"></i></div>
+        </a>
         <h4 class="title"><a href="<?=base_url('superior-service.html')?>">Layanan Unggulan</a></h4>
       </div>
       <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="100">
-        <div class="icon"><i class="fas fa-stethoscope"></i></div>
+        <a href="<?=base_url('outpatient-installation.html')?>">
+          <div class="icon"><i class="fas fa-stethoscope"></i></div>
+        </a>
         <h4 class="title"><a href="<?=base_url('outpatient-installation.html')?>">Instalasi Rawat Jalan</a></h4>
       </div>
       <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="200">
-        <div class="icon"><i class="fas fa-hospital"></i></div>
+        <a href="<?=base_url('inpatient-installation.html')?>">
+          <div class="icon"><i class="fas fa-hospital"></i></div>
+        </a>
         <h4 class="title"><a href="<?=base_url('inpatient-installation.html')?>">Instalasi Rawat Inap</a></h4>
       </div>
       <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="300">
-        <div class="icon"><i class="fas fa-ambulance"></i></div>
+        <a href="<?=base_url('igd.html')?>">
+          <div class="icon"><i class="fas fa-ambulance"></i></div>
+        </a>
         <h4 class="title"><a href="<?=base_url('igd.html')?>">Instalasi Gawat Darurat</a></h4>
       </div>
       <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="100">
-        <div class="icon"><i class="fas fa-dna"></i></div>
+        <a href="<?=base_url('laboratorium.html')?>">
+          <div class="icon"><i class="fas fa-dna"></i></div>
+        </a>
         <h4 class="title"><a href="<?=base_url('laboratorium.html')?>">Laboratorium</a></h4>
       </div>
       <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="200">
-        <div class="icon"><i class="fas fa-medkit"></i></div>
+        <a href="<?=base_url('radiology.html')?>">
+          <div class="icon"><i class="fas fa-medkit"></i></div>
+        </a>
         <h4 class="title"><a href="<?=base_url('radiology.html')?>">Radiology</a></h4>
       </div>
     </div>
