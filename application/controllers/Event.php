786b6f22df4bcbc93c46cc90a08b870f63e2d8b1
Ilham Akbari <ilham@eannovate.com>
2022-01-20 15:28:26 +0700

    Adjust. Event SEO

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -16,6 +16,11 @@ class Event extends CI_Controller {
 		$data['cur_page'] = 'event';
 		$data['cur_parent_page'] = '';
 
+		$data['seo_title'] = "RSD Cimacan | Event";
+		$data['seo_keyword'] = "Event Acara Agenda RSD Cimacan";
+		$data['seo_desc'] = 'Daftar Acara yang diadakan oleh Rumah Sakit Daerah Cimacan';
+		$data['seo_url'] = base_url().'event/';
+
 		$category = $this->input->get('category') ? $this->input->get('category') : '';
 		$s = $this->input->get('s') ? $this->input->get('s') : '';
 		$page = 1;
@@ -50,7 +55,8 @@ class Event extends CI_Controller {
 		$data['page'] = $page ? $page : 1;
 		$data['totalData'] = $totalData[0]->totalData;
 		$data['totalPage'] = $totalPage;
-		$data['datas'] = $this->T_Event->getPage($page, $category, $s);
+		$datas = $this->T_Event->getPage($page, $category, $s);
+		$data['datas'] = $datas;
 		$data['categories'] = $this->T_Event_Category->show_all();
 		$this->load->view('fe/event', $data);
 	}
@@ -60,7 +66,17 @@ class Event extends CI_Controller {
 		$data['cur_page'] = 'event';
 		$data['cur_parent_page'] = '';
 		$data['lang'] = 'id';
-		$data['datas'] = $this->T_Event->getDetail($id);
+		$datas = $this->T_Event->getDetail($id);
+
+		$data['seo_title'] = substr(strtoupper($datas[0]->title),0, 30).' | RSD Cimacan';
+		$data['seo_keyword'] = strtoupper($datas[0]->title);
+		$data['seo_desc'] = substr($datas[0]->description,0, 147).'...';
+
+		$lowerText = strtolower($datas[0]->title); 
+		$change_url = str_replace(' ', '-', $lowerText);
+		$data['seo_url'] = base_url().'event-'.$datas[0]->id.'-'.$change_url.'.html';
+
+		$data['datas'] = $datas;
 		$data['datas_other'] = $this->T_Event->getOther($id);
 		$this->load->view('fe/event_detail', $data);
 	}
