65a540d8993ceea0060e07b8da9685a97d9342d9
Ilham Akbari <ilham@eannovate.com>
2022-01-20 15:07:50 +0700

    Adjust . SEO Doctor

--- application/controllers/Doctor.php
+++ application/controllers/Doctor.php
@@ -17,6 +17,11 @@ class Doctor extends CI_Controller {
 		$data['cur_page'] = 'doctor';
 		$data['cur_parent_page'] = '';
 
+		$data['seo_title'] = "RSD Cimacan | Dokter";
+		$data['seo_keyword'] = "Dokter RSD Cimacan";
+		$data['seo_desc'] = 'Daftar dokter spesialis Rumah Sakit Daerah Cimacan';
+		$data['seo_url'] = base_url().'doctor/';
+
 		$field = $this->input->get('field') ? $this->input->get('field') : '';
 		$s = $this->input->get('s') ? $this->input->get('s') : '';
 		$page = 1;
@@ -26,6 +31,7 @@ class Doctor extends CI_Controller {
 		$data['field_selected'] = $field;
 		$data['s'] = $s;
 		$data['lang'] = 'id';
+
 		$data['page'] = 1;
 		$data['totalData'] = $totalData[0]->totalData;
 		$data['totalPage'] = $totalPage;
@@ -61,7 +67,18 @@ class Doctor extends CI_Controller {
 		$data['cur_page'] = 'doctor';
 		$data['cur_parent_page'] = '';
 		$data['lang'] = 'id';
-		$data['datas'] = $this->T_Doctor->getDetail($id);
+		$datas = $this->T_Doctor->getDetail($id);
+		$data['seo_title'] = strtoupper($datas[0]->name).' | RSD Cimacan';
+		$data['seo_keyword'] = strtoupper($datas[0]->name)." RSD Cimacan";
+		$data['seo_desc'] = $datas[0]->name.' dengan spesialisasi '.$datas[0]->field.' yang memiliki tanggung jawab di '.$datas[0]->office.' Rumah Sakit Daerah Cimacan';
+
+		 $lower_name = strtolower($datas[0]->name);
+	     $delete_dots = str_replace('.', ' ', $lower_name);
+	     $delete_coma = str_replace(',', ' ', $delete_dots);
+	     $fix_name = str_replace(' ', '-', $delete_coma);
+		$data['seo_url'] = base_url().'doctor-'.$datas[0]->id.'-'.$fix_name.'.html';
+
+		$data['datas'] = $datas;
 		$data['datas_other'] = $this->T_Doctor->getOther($id);
 		$data['schedules'] = $this->T_Schedule_Doctor->getDoctorDetail($id);
 		$this->load->view('fe/doctor_detail', $data);
