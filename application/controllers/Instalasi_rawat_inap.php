a1faadd1ad8e88046964ac036d3ff4042a557cbd
Ilham Akbari <ilham@eannovate.com>
2022-01-20 10:00:59 +0700

    Adjust. SEO Ranap

--- application/controllers/Instalasi_rawat_inap.php
+++ application/controllers/Instalasi_rawat_inap.php
@@ -16,6 +16,10 @@ class Instalasi_rawat_inap extends CI_Controller {
 		$data['cur_parent_page'] = 'layanan';
 		$data['lang'] = 'id';
 		$datas = $this->M_IRI->show_iri();
+		$data['seo_title'] = "RSD Cimacan | Instalasi Rawat Inap";
+		$data['seo_keyword'] = "Instalasi Rawat Inap RSD Cimacan";
+		$data['seo_desc'] = 'Instalasi Rawat Inap (IRI) merupakan instalasi yang memberikan pelayanan kesehatan perorangan dan perawatan yang meliputi observasi, pemeriksaan...';
+		$data['seo_url'] = base_url().'inpatient-installation.html';
 		$data['datas'] = $datas;
 		$data['galleries'] = $this->M_IRI->show_gallery('iri');
 		$data['sub_menus'] = $this->M_IRI->show_sub_menu($datas[0]->id);
