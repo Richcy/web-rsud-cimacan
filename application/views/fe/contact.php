6c485d0538f4772dc95109c713a0c5089526c419
Ilham Akbari <ilham@eannovate.com>
2022-01-21 14:52:21 +0700

    Add. SEO Contact

--- application/views/fe/contact.php
+++ application/views/fe/contact.php
@@ -43,9 +43,56 @@
   </div>
   <!-- End Hero -->
   <main id="main">
-    <?php 
-      $this->load->view('fe/parts/contact'); 
-    ?>
+    <section id="content" class="main-page">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h1 class="title-page">Kontak RSD Cimacan</h1>
+        </div>
+
+      </div>
+
+      <div>
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.403937234007!2d107.03033791384557!3d-6.720464567574099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c41c14b26fab%3A0x6230e0dec5dc2dd9!2sRumah%20Sakit%20Umum%20Daerah%20(RSUD)%20Cimacan!5e0!3m2!1sen!2sid!4v1637289497439!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+
+      <div class="container">
+
+        <div class="row mt-5">
+
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="info-box">
+                  <i class="bx bx-map"></i>
+                  <h3>Alamat Kami</h3>
+                  <p>Jl. Raya Cimacan No.17A, Palasari, Kec. Cipanas, Kabupaten Cianjur, Jawa Barat 43253</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-envelope"></i>
+                  <h3>Email Kami</h3>
+                  <p>rsudcimacan@gmail.com</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-phone-call"></i>
+                  <h3>Hotline WA</h3>
+                  <p>(+62) 8112031117</p>
+
+                  <!-- <h3>Ambulance</h3>
+                  <p>(+62)8112031117</p> -->
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
   </main>
   <!-- End #main -->
 
