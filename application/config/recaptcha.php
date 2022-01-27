9e13c44cc1a5d5230e449ff4afd620fe6a21f598
Ilham Akbari <ilham@eannovate.com>
2022-01-27 08:06:54 +0700

    Add site key for localhost

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // To use reCAPTCHA, you need to sign up for an API key pair for your site.
 // link: http://www.google.com/recaptcha/admin
-$config['recaptcha_site_key'] = '6LfbKzoeAAAAAAffzozDuu6QcJU0rBo-pQgPo3fT';
-$config['recaptcha_secret_key'] = '6LfbKzoeAAAAAARJaBrEFBus9dWeUSo39jjMSSMD';
+$config['recaptcha_site_key'] = '6Ld5CDoeAAAAAIznE6jcIGzkeMiTDUsmOpJzJtIr';
+$config['recaptcha_secret_key'] = '6Ld5CDoeAAAAAMXRCubIlMeUOiLGOcxVJXuG1bHu';
 
 // reCAPTCHA supported 40+ languages listed here:
 // https://developers.google.com/recaptcha/docs/language
