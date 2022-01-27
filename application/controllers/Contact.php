8c5994f3a4fa25d21c9b20c757f4f3cac23ae992
Ilham Akbari <ilham@eannovate.com>
2022-01-27 12:05:55 +0700

    Add session in contact controller

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contact extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+        $this->load->library('session');
+	}
+
 	public function index()
 	{
 		$data['cur_page'] = 'contact';
