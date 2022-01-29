7d7aa4bef1bf919510c0b1df12d5debf782072f7
Ilham Akbari <ilham@eannovate.com>
2022-01-29 11:08:28 +0700

    add mtrand

--- application/views/fe/packages/head.php
+++ application/views/fe/packages/head.php
@@ -1,51 +1,101 @@
 <meta charset="utf-8">
+
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
   <meta name="robots" content="index, follow">
+
   <meta name="googlebot" content="index, follow" />
+
   <meta name="google-site-verification" content="vWAgVaWw63x8fSarefmtTOdB2iC2TkfNO193gvNCusQ" />
 
+
+
   <!-- Favicons -->
+
   <link href="<?php echo base_url();?>assets/fe/img/logo_rsud_cimacan.png" rel="icon">
 
+
+
   <!-- Google Fonts -->
+
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+
+
   <!-- Vendor CSS Files -->
-  <link href="<?php echo base_url();?>assets/fe/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/aos/aos.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/animate.css/animate.min.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/aos/aos.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/bootstrap/css/bootstrap.min.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/bootstrap-icons/bootstrap-icons.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/boxicons/css/boxicons.min.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/fontawesome-free/css/all.min.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/glightbox/css/glightbox.min.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/vendor/swiper/swiper-bundle.min.css?<?=mt_rand();?>" rel="stylesheet">
+
+
 
   <!-- Template Main CSS File -->
-  <link href="<?php echo base_url();?>assets/fe/css/style.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/css/style-fe-plus.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/fe/css/style-fe.css" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/css/style.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/css/style-fe-plus.css?<?=mt_rand();?>" rel="stylesheet">
+
+  <link href="<?php echo base_url();?>assets/fe/css/style-fe.css?<?=mt_rand();?>" rel="stylesheet">
+
+
 
   <!-- External -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fe/css/datatables.min.css"/>
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/fe/css/jquery-ui.css">
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fe/css/datatables.min.css?<?=mt_rand();?>"/>
+
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/fe/css/jquery-ui.css?<?=mt_rand();?>">
+
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css?<?=mt_rand();?>" rel="stylesheet" />
+
+
 
   <!-- Analytics -->
+
   <!-- Google Tag Manager -->
-  <<!-- script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+<!--   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
   })(window,document,'script','dataLayer','GTM-T23G88R');</script> -->
+
   <!-- End Google Tag Manager -->
 
+
+
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-47KKZP9ZXN"></script>
+<!-- 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-47KKZP9ZXN"></script>
+
   <script>
+
     window.dataLayer = window.dataLayer || [];
+
     function gtag(){dataLayer.push(arguments);}
+
     gtag('js', new Date());
 
+
+
     gtag('config', 'G-47KKZP9ZXN');
+
   </script> -->
