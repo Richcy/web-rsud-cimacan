a47cec9c4a670e67dbd8dcb7bbc832fdf1394737
Richcy Dian Sukma <richcyveliz@gmail.com>
2023-07-29 11:08:32 +0700

    Learning CodeIgniter 3: Styling home pengaduan section

--- application/views/fe/parts/pengaduan.php
+++ application/views/fe/parts/pengaduan.php
@@ -7,42 +7,36 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-4 order-2 order-lg-1" data-aos="fade-right">
+          <div class="col-lg-4 order-2 order-lg-1 mt-2" data-aos="fade-right">
             <div class="complain">
-              <div class="icon-box mt-3">
+              <div class="icon-box">
                 <i class="bx bx-user"></i>
-                <h4>Customer Services</h4>
-                <p>Tatap muka secara offline</p>
+                <p style="margin-top: 0px; margin-bottom: -10px;">Customer Service</p>
+                <p>(Gedung B Lantai 3)</p>
               </div>
-              <div class="icon-box mt-3">
-                <img src="assets/uploads/image/sp4n.png" width="48" height="48" style="float:left;">
-                <h4>SP4N LAPOR</h4>
-                <p><a href="https://www.lapor.go.id/">Link SP4N LAPOR</a></p>
+              <div class="icon-box">
+                <img src="assets/uploads/image/sp4n.png">
+                <p><a href="https://www.lapor.go.id/">SP4N LAPOR</a></p>
               </div>
-              <div class="icon-box mt-3">
+              <div class="icon-box">
                 <i class="bx bxl-whatsapp"></i>
-                <h4>WhatsApp</h4>
                 <p><a href="https://wa.me/6285864817874?text=Halo%20Kak%20.%20.%20." target="_blank">(+62) 858-6481-7874</a></p>
               </div>
-              <div class="icon-box mt-3">
+              <div class="icon-box">
                 <i class="bx bx-phone"></i>
-                <h4>Operator Telepon</h4>
-                <p>0263 2956 036</p>
+                <p>0263-2956-036</p>
               </div>
-                <div class="icon-box mt-3">
+                <div class="icon-box">
                 <i class="bx bxl-instagram"></i>
-                <h4>Instagram</h4>
-                <p><a href="https://www.instagram.com/rsud.cimacan/">Link Instagram</a></p>
+                <p><a href="https://www.instagram.com/rsud.cimacan/">Instagram</a></p>
               </div>
-                <div class="icon-box mt-3">
+                <div class="icon-box">
                 <i class="bx bx-envelope"></i>
-                <h4>Email</h4>
                 <p>rsud.cimacann@gmail.com</p>
               </div>
-               <div class="icon-box mt-3">
+               <div class="icon-box">
                 <i class="bx bxl-facebook"></i>
-                <h4>Facebook</h4>
-                <p><a href="https://www.facebook.com/profile.php?id=100071691815827">Link Facebook</a></p>
+                <p><a href="https://www.facebook.com/profile.php?id=100071691815827">Facebook</a></p>
               </div>
             </div>
           </div>
